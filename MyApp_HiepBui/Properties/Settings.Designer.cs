521c2db192c2732a1cc3d8abe58e0b02e4aca63d
Vu Xuan Hoang <89919829+snowcatP@users.noreply.github.com>
2023-05-17 15:56:38 +0700

    Revert "Hoang branch"

--- MyApp_HiepBui/Properties/Settings.Designer.cs
+++ MyApp_HiepBui/Properties/Settings.Designer.cs
@@ -12,9 +12,7 @@ namespace MyApp_HiepBui.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
-
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -28,13 +26,11 @@ namespace MyApp_HiepBui.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=HOANGPRO\\HOANGPRO;Initial Catalog=ConvenienceStoreManagement;Integrat" +
-            "ed Security=True")]
-        public string ConvenienceStoreManagementConnectionString {
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=HIEPBUI2812\\SQLEXPRESS;Initial Catalog=ConvenienceStoreManagement(new" +
+            ");Integrated Security=True")]
+        public string ConvenienceStoreManagement_new_ConnectionString {
             get {
-                return ((string)(this["ConvenienceStoreManagementConnectionString"]));
-
+                return ((string)(this["ConvenienceStoreManagement_new_ConnectionString"]));
             }
         }
     }
